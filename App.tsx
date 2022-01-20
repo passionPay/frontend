8180063754ba00bcac49408b90a70440184e8b70
yoonyeseul <yoonyesle@naver.com>
2022-01-20 17:17:06 +0900

    react-native-animated-nav-tab-bar 에러 없이 실행되는 버전

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { AnimatedTabBarNavigator } from "react-native-animated-nav-tab-bar";
+import { NavigationContainer } from '@react-navigation/native';
+import { Home, Home2, Home3, Home4 } from './src/Home';
 
 export default function App() {
-  return <Text>에러 없이 실행되는 버전</Text>
+  const Tab = AnimatedTabBarNavigator();
+  return <NavigationContainer>
+    <Tab.Navigator appearance={{topPadding: 20}}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Home2" component={Home2} />
+      <Tab.Screen name="Home3" component={Home3} />
+      <Tab.Screen name="Home4" component={Home4} />
+    </Tab.Navigator>
+  </NavigationContainer>
 }
---
+++ src/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export function Home() {
+    return <Text>홈</Text>
+}
+
+
+export function Home2() {
+    return <Text>홈</Text>
+}
+
+export function Home3() {
+    return <Text>홈</Text>
+}
+
+export function Home4() {
+    return <Text>홈</Text>
+}
